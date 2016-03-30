dd013b7d7b3a7cce49a85a7a4f443e4efbda98fd
Julian Ospald <hasufell@posteo.de>
2016-03-31 00:28:23 +0200

    Fix Copyright

--- src/HSFM/FileSystem/FileOperations.hs
+++ src/HSFM/FileSystem/FileOperations.hs
@@ -1,6 +1,6 @@
 {--
 HSFM, a filemanager written in Haskell.
-Copyright (C) 2015 Julian Ospald
+Copyright (C) 2016 Julian Ospald
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
